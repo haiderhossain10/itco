3a766749092b8dc15cbbae66d40782b8a1f76581
Md Haider Hossain <mdhaiderhossain10@gmail.com>
2024-03-01 15:21:50 +0600

    service details done

---
+++ app/(services)/service-details/page.tsx
@@ -0,0 +1,36 @@
+import Footer from "@/components/footer/footer";
+import Header from "@/components/header/header";
+import ServiceInfo from "@/components/pages/(services)/service-info";
+import Breadcumb from "@/components/ui/breadcumbs/breadcumb";
+import Subscribe from "@/components/ui/sections/subscribe";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Service Details",
+};
+
+const breadcumb_data = {
+    title: "Service Details",
+    list: [
+        {
+            name: "Home",
+            url: "/",
+        },
+        {
+            name: "Service Details",
+            url: "/service-details",
+        },
+    ],
+};
+
+export default function ServiceDetails() {
+    return (
+        <>
+            <Header />
+            <Breadcumb data={breadcumb_data} />
+            <ServiceInfo />
+            <Subscribe />
+            <Footer />
+        </>
+    );
+}
